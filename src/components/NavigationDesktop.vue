6625b5414c8b8cddd87b96f30d424b354d69f974
pabloberry44 <pawel.borowiec@tutanota.com>
2022-12-24 15:17:28 +0100

    mobile and desktop navigation

---
+++ src/components/NavigationDesktop.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav>
+        <img src="../assets/logo.svg" alt="Trafalgar" />
+        <ul>
+            <li>Home</li>
+            <li>Find a doctor</li>
+            <li>Apps</li>
+            <li>Testimonials</li>
+            <li>About us</li>
+        </ul>
+    </nav>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+nav {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: space-between;
+    padding: 56px 10vw;
+
+    ul {
+        display: flex;
+        flex-flow: row nowrap;
+        list-style: none;
+        gap: 40px;
+        font-size: 18px;
+        font-weight: 400;
+
+        li {
+            cursor: pointer;
+            opacity: 0.5;
+            transition: opacity 300ms;
+
+            &:hover {
+                opacity: 1;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/NavigationMobile.vue
@@ -0,0 +1,120 @@
+<template>
+    <nav>
+        <div class="nav-container">
+            <img src="../assets/logo.svg" alt="Trafalgar" />
+
+            <button @click="menuIsVisible = !menuIsVisible">
+                <div class="wrapper" :class="{ trans: menuIsVisible }">
+                    <div class="menu-line" :class="{ line1: menuIsVisible }"></div>
+                    <div class="menu-line" :class="{ line2: menuIsVisible }"></div>
+                    <div class="menu-line" :class="{ line3: menuIsVisible }"></div>
+                </div>
+            </button>
+        </div>
+        <transition name="height">
+            <div class="dropdown" v-if="menuIsVisible">
+                <ul>
+                    <li>Home</li>
+                    <li>Find a doctor</li>
+                    <li>Apps</li>
+                    <li>Testimonials</li>
+                    <li>About us</li>
+                </ul>
+            </div>
+        </transition>
+    </nav>
+    <div class="overlay" v-if="menuIsVisible" @click="menuIsVisible = false"></div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const menuIsVisible = ref(false)
+</script>
+<style scoped lang="scss">
+nav {
+    display: flex;
+    flex-direction: column;
+    font-size: 18px;
+    position: relative;
+
+    .nav-container {
+        width: 100%;
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        justify-content: space-between;
+        padding: 30px 7vw;
+        position: relative;
+        z-index: 10;
+        background: #ffffff;
+
+        img {
+            height: 41px;
+        }
+
+        button {
+            .wrapper {
+                display: flex;
+                flex-direction: column;
+                gap: 8px;
+                transition: 500ms transform;
+
+                &.trans {
+                    transform: translateX(50%);
+                }
+
+                .menu-line {
+                    width: 30px;
+                    height: 2px;
+                    background: black;
+                    transition: transform 500ms;
+
+                    &.line1 {
+                        transform: rotateZ(-45deg) translateX(-50%);
+                    }
+                    &.line2 {
+                        transform: rotateZ(-45deg) translate(-8px, -7px);
+                        height: 2px;
+                    }
+                    &.line3 {
+                        transform: rotateZ(45deg) translateX(-47%);
+                    }
+                }
+            }
+        }
+    }
+
+    .dropdown {
+        background: #ffffff;
+        padding: 30px 7vw;
+        z-index: 9;
+        position: absolute;
+        top: 101px;
+        width: 100%;
+
+        ul {
+            display: flex;
+            flex-flow: column;
+            list-style: none;
+            gap: 20px;
+        }
+    }
+}
+.overlay {
+    width: 100%;
+    height: 100vh;
+    z-index: 8;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+.height-enter-active,
+.height-leave-active {
+    transition: transform 0.5s ease;
+}
+
+.height-enter-from,
+.height-leave-to {
+    transform: translateY(-100%);
+}
+</style>
